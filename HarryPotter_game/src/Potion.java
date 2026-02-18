b9d0ffbc9c8a0604e93fe16aa3b7b825f0b5375c
Aslı Zeynep Akhan <zeynepakhan04@icloud.com>
2026-02-18 17:04:15 +0300

    Add files via upload
     The Story: Shadows in the Great Hall
    The torches in the Great Hall flicker as two rivals face each other on the dueling platform. On
    one side stands Harry, a brave member of House Gryffindor; on the other, the formidable
    Voldemort of House Slytherin. Each wizard carries a unique wand that hums with its own
    power. Some wands, like those with a Dragon Heartstring core, are known to be the most
    powerful, while Phoenix Feather cores are balanced and reliable.
    To win, a wizard must manage their Spellbook and their Pocket. A spellbook contains
    various incantations: some are high-damage but difficult to land, while others are quick and
    highly accurate. In the heat of battle, a wizard might reach into their pocket for a potion to
    heal their wounds, though they can only carry a limited supply.
    The duel is a turn-based dance of strategy. A wizard must decide whether to cast a
    spell—hoping their wand's power and the spell's base damage are enough to overcome the
    opponent—or to use a precious turn to drink a potion. The duel only ends when one wizard's
    health finally hits zero

---
+++ HarryPotter_game/src/Potion.java
@@ -0,0 +1,23 @@
+public class Potion {
+
+    private String name;
+    private int healAmount;
+
+
+    Potion(String name, int healAmount) {
+        this.name = name;
+        this.healAmount = healAmount;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getHealAmount() {
+        return healAmount;
+    }
+
+
+
+
+}
